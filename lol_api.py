cdddc1a75ff411c9493256336a35ab6285abd764
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-09-24 17:39:54 +0100

    got champion details
    champion, title, ally tips, enemy tips, lore, skins and  skin images, and abilities

--- lol_api.py
+++ lol_api.py
@@ -1,12 +1,59 @@
 import requests
 from pprint import pprint
 
-def champion():
+
+skin_img_url = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'
+def lol_champion():
     url = 'https://ddragon.leagueoflegends.com/cdn/14.18.1/data/en_US/champion.json'
     response = requests.get(url)
-    champions = response.json()['data']
-    pprint(champions)
+    data = response.json()['data']
 
-    
+    champion_data = []
+    for champ in data:
+        champion_url = f'https://ddragon.leagueoflegends.com/cdn/14.18.1/data/en_US/champion/{champ}.json'
+        champion_response = requests.get(champion_url)
+        champion = champion_response.json()['data'][champ]
 
-champion()
+        title = champion['title']
+        allytips = champion['allytips']
+        enemytips = champion['enemytips']
+        print(enemytips)
+        story = champion['lore']
+
+        skins = champion['skins']
+        champ_skins = []
+        for skin in skins:
+            skin_name = champ
+            if skin['name'] != 'default':
+                skin_name = skin['name']
+
+            champ_skins.append({
+                'name': skin_name,
+                'image': f'{skin_img_url}{champ}_{skin['num']}.jpg'
+            })
+
+        abilities = champion['spells']
+        champ_abilities = []
+        for ability in abilities:
+            ability_id = ability['id']
+            ability_name = ability['name']
+            ability_description = ability['description']
+            champ_abilities.append({
+                'id': ability_id,
+                'ability': ability_name,
+                'description': ability_description
+            })
+
+        champion_data = [ {
+            '2024': {
+                'champion': champion,
+                'title': title,
+                'story': story,
+                'skins': champ_skins,
+                'abilities': champ_abilities
+
+            }}]
+
+        break
+
+lol_champion()
